b4c61255875eda74e427d7979a23d32c133220c2
jenkin <jenkinpan0628@gmail.com>
2024-04-16 19:03:45 +0800

    init repo

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterView } from "vue-router";
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<template>
+  <main class="app">
+    <Sidebar />
+    <RouterView />
+  </main>
+</template>
+
+<style lang="scss">
+:root {
+  --primary: #4ade80;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira sans", sans-serif;
+}
+
+body {
+  background: var(--light);
+}
+
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width: 768px) {
+      padding-left: 6rem;
+    }
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/Home.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/About.vue"),
+    },
+    {
+      path: "/todo",
+      name: "todo",
+      component: () => import("../views/TodoList.vue"),
+    },
+  ],
+});
+
+export default router;
